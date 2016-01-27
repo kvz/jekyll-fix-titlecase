1df13ef70fccdf6cc02af5a14c669c6a0f4f954c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-01-27 14:41:24 +0100

    Implement async

--- bin/jekyll-fix-titlecase.js
+++ bin/jekyll-fix-titlecase.js
@@ -3,12 +3,14 @@ var cli          = require('cli').enable('status', 'help', 'version', 'glob', 't
 var fs           = require('fs');
 var glob         = require('glob');
 var path         = require('path');
-var fixTitlecase = require('./lib/fix-titlecase');
+var async        = require('async');
+var fixTitlecase = require('../lib/fix-titlecase');
 
 // --debug works out of the box. See -h
 cli.parse({
-  dir : ['dir', 'Directory with markdown posts to convert', 'path'],
-  title : ['title', 'Just one title as cli arg', 'string']
+  dir   : ['dir', 'Directory with markdown posts to convert', 'path'],
+  title : ['title', 'Just one title as cli arg', 'string'],
+  fix   : ['fix', 'Try to fix Markdown blog posts automatically (warning: overwrites existing files)', 'boolean', false],
 });
 
 cli.main(function(args, options) {
@@ -25,32 +27,57 @@ cli.main(function(args, options) {
 
   var pattern = options.dir + '/*.{markdown,md}';
   self.debug('Scanning ' + pattern);
+  var issuesFound = 0;
+  var issuesFixed = 0;
   glob(pattern, {}, function(err, files) {
     if (err) {
-      return self.error(err);
+      throw new Error(err);
     }
 
-    files.forEach(function (filepath) {
-      fs.readFile(filepath, 'utf-8', function(err, content) {
+    var q = async.queue(function(filePath, cb){
+      fs.readFile(filePath, 'utf-8', function(err, content) {
         if (err) {
-          return self.error(err);
+          throw new Error(err);
         }
 
         fixTitlecase.newPost(content, function (err, newPost, oldTitle, newTitle) {
           if (err) {
-            return self.error(err);
-          }
-          if (newPost === null) {
-            self.debug('No need to change ' + filepath + '\n');
-            return;
+            throw new Error(err);
           }
 
-          self.info('Rewriting ' + filepath);
-          self.debug(' from ' + oldTitle);
-          self.debug('   to ' + newTitle + '\n');
-          fs.writeFile(filepath, newPost);
+          if (newPost === null) {
+            self.debug('No need to change ' + filePath);
+            return cb();
+          }
+
+          if (options.fix === true) {
+            self.info('Rewriting ' + filePath);
+          } else {
+            self.info(' Issue in ' + filePath);
+          }
+          self.debug('          ' + oldTitle);
+          self.debug('should be ' + newTitle);
+          issuesFound++;
+          if (options.fix) {
+            fs.writeFileSync(filePath, newPost);
+            issuesFixed++;
+          }
+
+          return cb();
         });
       });
-    });
+    }, 1);
+    q.drain = function() {
+      self.info(issuesFound + ' issues found');
+      self.info(issuesFixed + ' issues fixed');
+
+      var issuesRemain = issuesFound - issuesFixed;
+      if (issuesRemain > 0) {
+        process.exit(1);
+      }
+      process.exit(0);
+    };
+    q.push(files);
+
   });
 });
